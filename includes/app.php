a007708bbd0a5065402caf39c3a6953c3f3f8298
Mario Alejandro Jimenez <cuentas.alexjimenez@gmail.com>
2022-05-23 08:19:07 -0500

    Version 0.0.16 No estable

--- includes/app.php
+++ includes/app.php
@@ -1,12 +1,14 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env' );
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 require 'funciones.php';
 require 'database.php';
 
-use Model\ActiveRecord;
+    $db = conectarDb();
 
-ActiveRecord::setDB($db);
+    use Model\ActiveRecord;
+
+    ActiveRecord::setDB($db);
